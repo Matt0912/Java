ab5b263b3ffb2b79d145f9ac4b902e091c590890
Matt0912 <matt.meades01@gmail.com>
2020-02-17 13:07:59 +0000

    OXO Scaled up for any nxn grid

--- Assignments/03-OXO/src/OXOController.java
+++ Assignments/03-OXO/src/OXOController.java
@@ -2,11 +2,15 @@ class OXOController
 {
     private OXOModel model;
     private int turns = 0;
+    private int numRows;
+    private int numCols;
 
     public OXOController(OXOModel model)
     {
         this.model = model;
         model.setCurrentPlayer(model.getPlayerByNumber(0));
+        numRows = model.getNumberOfRows();
+        numCols = model.getNumberOfColumns();
 
     }
 
@@ -14,43 +18,89 @@ class OXOController
     {
         // If the game is over, don't accept any more user input
         if (model.getWinner() != null || model.isGameDrawn()) return;
-        if (command.length() != 2) {
-            System.out.println(new InvalidCellIdentifierException("length of input", command).toString());
-            return;
+        if (command.length() == 2) {
+            int x = command.toLowerCase().charAt(0) - 'a';
+            int y = command.charAt(1) - '0' - 1;
+            if (checkValidInput(command, x, y)) {
+                turns++;
+                model.setCellOwner(x, y, model.getCurrentPlayer());
+                model.setCurrentPlayer(model.getPlayerByNumber(turns % model.getNumberOfPlayers()));
+
+                checkWinConditions();
+            }
         }
-        int x = command.toLowerCase().charAt(0) - 'a';
-        int y = command.charAt(1) - '0' - 1;
-        if (x < 0 ||  x > model.getNumberOfColumns() - 1 || y < 0 ||  y > model.getNumberOfRows() - 1) {
+        else {
+            System.out.println(new InvalidCellIdentifierException("length of input", command).toString());
+        }
+    }
+
+    public boolean checkValidInput(String input, int x, int y) {
+        if (x < 0 ||  x > numCols - 1 || y < 0 ||  y > numRows - 1) {
             System.out.println(new CellDoesNotExistException(x,y).toString());
         }
         else if (model.getCellOwner(x,y) != null) {
             System.out.println(new CellAlreadyTakenException(x,y).toString());
         }
         else {
-            turns++;
-            model.setCellOwner(x, y, model.getCurrentPlayer());
-            model.setCurrentPlayer(model.getPlayerByNumber(turns % model.getNumberOfPlayers()));
-
-            checkWinConditions();
-
+            return true;
         }
+        return false;
     }
 
     public void checkWinConditions() {
-        for (int i = 0; i < model.getNumberOfColumns(); i++) {
-            if (model.getCellOwner(i, 0) == model.getCellOwner(i, 1) &&
-                    model.getCellOwner(i, 1) == model.getCellOwner(i, 2)) {
-                model.setWinner(model.getCellOwner(i,0));
-            }
-        }
-        for (int i = 0; i < model.getNumberOfRows(); i++) {
-            if (model.getCellOwner(0, i) == model.getCellOwner(1, i) &&
-                    model.getCellOwner(1, i) == model.getCellOwner(2, i)) {
-                model.setWinner(model.getCellOwner(0, i));
+        OXOPlayer currOwner;
+        int i, j;
+        // Check for win in columns
+        for (i = 0; i < numCols; i++) {
+            currOwner = model.getCellOwner(0, i);
+            j = 1;
+            while (currOwner == model.getCellOwner(j, i)) {
+                if (j == numRows - 1) {
+                    model.setWinner(currOwner);
+                    return;
+                }
+                j++;
             }
         }
 
-        if (turns == model.getNumberOfRows()*model.getNumberOfColumns()) {
+        // Check for win in rows
+        for (i = 0; i < numRows; i++) {
+            currOwner = model.getCellOwner(i, 0);
+            j = 1;
+            while (currOwner == model.getCellOwner(i, j)) {
+                if (j == numCols - 1) {
+                    model.setWinner(currOwner);
+                    return;
+                }
+                j++;
+            }
+        }
+
+        // Check for diagonal wins
+        currOwner = model.getCellOwner(0, numCols - 1);
+        i = 1;
+        j = numCols - 1 - i;
+        while (currOwner == model.getCellOwner(i, j)) {
+            if (j == 0) {
+                model.setWinner(currOwner);
+                return;
+            }
+            i++;
+            j--;
+        }
+        currOwner = model.getCellOwner(0, 0);
+        i = 1;
+        j = 1;
+        while (currOwner == model.getCellOwner(i, j)) {
+            if (j == numCols - 1) {
+                model.setWinner(currOwner);
+                return;
+            }
+            i++;
+            j++;
+        }
+
+        if (turns == numRows*numCols) {
             model.setGameDrawn();
         }
 
